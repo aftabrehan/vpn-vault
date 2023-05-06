8cd7f7524f0ac25c5839866378abae2385edb46c
aftabrehan <aftabrehan52422@gmail.com>
2023-05-06 14:41:09 +0500

    Add Pricing and Testimonial Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,13 @@ import type { NextPage } from 'next';
 import Layout from '../layout';
 import Hero from '../components/hero';
 import Feature from '../components/feature';
+import Pricing from '../components/pricing';
 
 const Home: NextPage = () => (
   <Layout>
     <Hero />
     <Feature />
+    <Pricing />
   </Layout>
 );
 
