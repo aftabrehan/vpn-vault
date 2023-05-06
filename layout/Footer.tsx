27eae06741e28c4deba4216c5fd9d97540e4c585
aftabrehan <aftabrehan52422@gmail.com>
2023-05-06 14:47:01 +0500

    Fix Footer

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -10,45 +10,44 @@ const Footer = () => (
   <div className="bg-white-300 pt-44 pb-24">
     <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
       <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
-        <LogoVPN className="h-8 w-auto mb-6" />
+        <LogoVPN className="h-10 w-auto" />
         <p className="mb-4">
-          <span className="font-medium">LaslesVPN</span>
-          is a private virtual network that has unique features and has high
-          security.
+          <span className="font-medium">VPN Vault</span> is a private virtual
+          network that has unique features and has high security.
         </p>
         <div className="flex w-full mt-2 mb-8 -mx-2">
-          <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+          <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md cursor-pointer hover:bg-primary-200">
             <Facebook className="h-6 w-6" />
           </div>
-          <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+          <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md cursor-pointer hover:bg-primary-200">
             <Twitter className="h-6 w-6" />
           </div>
-          <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+          <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md cursor-pointer hover:bg-primary-200">
             <Instagram className="h-6 w-6" />
           </div>
         </div>
-        <p className="text-gray-400">©{new Date().getFullYear()} - LaslesVPN</p>
+        <p className="text-gray-400">©{new Date().getFullYear()} - VPN Vault</p>
       </div>
       <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
         <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
         <ul className="text-black-500 ">
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Download{' '}
+            Download
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Pricing{' '}
+            Pricing
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Locations{' '}
+            Locations
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Server{' '}
+            Server
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Countries{' '}
+            Countries
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Blog{' '}
+            Blog
           </li>
         </ul>
       </div>
@@ -56,22 +55,22 @@ const Footer = () => (
         <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
         <ul className="text-black-500">
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            LaslesVPN ?{' '}
+            VPN Vault?
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            FAQ{' '}
+            FAQ
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Tutorials{' '}
+            Tutorials
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            About Us{' '}
+            About Us
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Privacy Policy{' '}
+            Privacy Policy
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Terms of Service{' '}
+            Terms of Service
           </li>
         </ul>
       </div>
@@ -79,10 +78,10 @@ const Footer = () => (
         <p className="text-black-600 mb-4 font-medium text-lg">Earn Money</p>
         <ul className="text-black-500">
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Affiliate{' '}
+            Affiliate
           </li>
           <li className="my-2 hover:text-primary-400 cursor-pointer transition-all">
-            Become Partner{' '}
+            Become Partner
           </li>
         </ul>
       </div>
