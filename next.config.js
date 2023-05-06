18c7bd7103a5df6302d390b632ea89cf706ebb13
aftabrehan <aftabrehan52422@gmail.com>
2023-05-06 17:13:21 +0500

    Fix Images

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   reactStrictMode: true,
-  images: { unoptimized: true },
+  // images: { unoptimized: true },
   webpack: (config) => {
     config.resolve.modules.push(path.resolve('./'));
     config.module.rules.push({ test: /\.svg$/, use: ['@svgr/webpack'] });
